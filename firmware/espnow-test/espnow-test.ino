bccc6d62c34bd2fab6330b9643cb4a1d6a4ecd5f
Ralf Mayet <trilly@gmail.com>
2021-04-28 12:06:30 +0200

    working receiving

--- firmware/espnow-test/espnow-test.ino
+++ firmware/espnow-test/espnow-test.ino
@@ -1,5 +1,6 @@
 #include <esp_now.h>
 #include <WiFi.h>
+#include <esp_wifi_internal.h>
 
 
 //######_ESPNOW_######
@@ -13,89 +14,102 @@ struct_message outputData;
 esp_now_peer_info_t peerInfo;
 
 // Master Controller Address
-uint8_t broadcastAddress[] = {0x24, 0x6F, 0x28, 0x51, 0xED, 0xA4};
+uint8_t broadcastAddress[] = {0xE8, 0x94, 0xF6, 0x27, 0xD1, 0xE6};
 
-
-
+//------------------------------------------------------------------------------------------
 
 void setup() {
+  Serial.begin(115200);
+  Serial.println("Hello, this is ESPNOW Test Code.");
   espNowInit();
 
 }
 
 void loop() {
 
-  
+  sendData();
+  delay(1000);
 }
 
 
 //------------------------------------------------------------------------------------------
+#define TRY_ESP_ACTION(action, name) if(action == ESP_OK) {Serial.println("\t+ "+String(name));} else {Serial.println("----------Error while " + String(name) + " !---------------");}
+
+#define CHANNEL 1
+#define DATARATE WIFI_PHY_RATE_24M
 
 void espNowInit(){
   // Init ESP-NOW
+  WiFi.disconnect();
   WiFi.mode(WIFI_STA);
+
   Serial.println(WiFi.macAddress());
-  if (esp_now_init() != ESP_OK) {
-    Serial.println("Error initializing ESP-NOW");
-    return;
-  }
+
+  TRY_ESP_ACTION( esp_wifi_stop(), "stop WIFI");
+  TRY_ESP_ACTION( esp_wifi_deinit(), "De init");
+  wifi_init_config_t my_config = WIFI_INIT_CONFIG_DEFAULT();
+  my_config.ampdu_tx_enable = 0;
+  TRY_ESP_ACTION( esp_wifi_init(&my_config), "Disable AMPDU");
+  TRY_ESP_ACTION( esp_wifi_start(), "Restart WiFi");
+  TRY_ESP_ACTION( esp_wifi_set_promiscuous(true), "Set promiscuous");
+  TRY_ESP_ACTION( esp_wifi_set_channel(CHANNEL, WIFI_SECOND_CHAN_NONE), "Set channel");
+  TRY_ESP_ACTION( esp_wifi_internal_set_fix_rate(ESP_IF_WIFI_STA, true, DATARATE), "Fixed rate set up");
+  TRY_ESP_ACTION( esp_now_init(), "ESPNow Init");
   
   esp_now_register_recv_cb(OnDataRecv);
-
   esp_now_register_send_cb(OnDataSent);
 
   // Register peer
   //esp_now_peer_info_t peerInfo;
   memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 0;  
+  peerInfo.channel = CHANNEL;  
   peerInfo.encrypt = false;
 
-  // Add peer        
-  if (esp_now_add_peer(&peerInfo) != ESP_OK){
-    Serial.println("Failed to add peer");
-    return;
-  }
+  // Add peer
+  TRY_ESP_ACTION( esp_now_add_peer(&peerInfo), "Add peer");
 }
 
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   memcpy(&inputData, incomingData, sizeof(inputData));
-  //Serial.print("Bytes received: ");
-  //Serial.println(len);
-  //Serial.print("Function: ");
-  //Serial.println(inputData.function);
-  //Serial.print("Value: ");
-  //Serial.println(inputData.value);
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  Serial.print("Function: ");
+  Serial.println(inputData.function);
+  Serial.print("Value: ");
+  Serial.println(inputData.value);
 
-  if (inputData.function == "pos"){
-    sendData();
-  }
-  else if(inputData.value == 0){
-    String espNowInput = inputData.function;
-    char wirelessCommand[10]; 
-    espNowInput.toCharArray(wirelessCommand, sizeof(wirelessCommand));
-    //commandEspNow.run(wirelessCommand);
-    Serial.println(wirelessCommand);
-  }
-  else{
-    String espNowInput = inputData.function + String(inputData.value,3);
-    char wirelessCommand[10]; 
-    espNowInput.toCharArray(wirelessCommand, sizeof(wirelessCommand));
-    //commandEspNow.run(wirelessCommand);
-    Serial.println(wirelessCommand);
-  }
+//  if (inputData.function == "pos"){
+//    sendData();
+//  }
+//  else if(inputData.value == 0){
+//    String espNowInput = inputData.function;
+//    char wirelessCommand[10]; 
+//    espNowInput.toCharArray(wirelessCommand, sizeof(wirelessCommand));
+//    //commandEspNow.run(wirelessCommand);
+//    Serial.println(wirelessCommand);
+//  }
+//  else{
+//    String espNowInput = inputData.function + String(inputData.value,3);
+//    char wirelessCommand[10]; 
+//    espNowInput.toCharArray(wirelessCommand, sizeof(wirelessCommand));
+//    //commandEspNow.run(wirelessCommand);
+//    Serial.println(wirelessCommand);
+//  }
   
 }
 
 void sendData(){
+  Serial.println("Sending some data");
   outputData.function = "A";
   outputData.value = 42.0;
   esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &outputData, sizeof(outputData));
+  Serial.println(result);
 }
 
 // Callback when data is sent
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  //Serial.print("Send Status:\t");
-  //Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+//  Serial.print("Send Status:\t");
+//  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
   /*if (status == 0){
     String success = "Delivery Success :)";
   }
