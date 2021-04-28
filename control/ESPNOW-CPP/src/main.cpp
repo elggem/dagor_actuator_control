fc89b6c73564762b6a0ac081b256bfc44170962d
Ralf Mayet <trilly@gmail.com>
2021-04-28 13:22:32 +0200

    working receiving messages

--- control/ESPNOW-CPP/src/main.cpp
+++ control/ESPNOW-CPP/src/main.cpp
@@ -20,7 +20,7 @@ using namespace std;
 
 //######_ESPNOW_######
 typedef struct struct_message {
-    char* function;
+    char function;
     float value;
 } struct_message;
 
@@ -29,19 +29,25 @@ struct_message outputData;
 
 static uint8_t my_mac[6] = {0xE8, 0x94, 0xF6, 0x27, 0xD1, 0xE6};
 static uint8_t dest_mac[6] = {0x24, 0x0A, 0xC4, 0x60, 0x0B, 0x5C};
-static uint8_t broadcastAddress[6] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
+// static uint8_t broadcastAddress[6] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
 
 ESPNOW_manager *handler;
 
-uint8_t payload[127];
+void callback(uint8_t src_mac[6], uint8_t *incomingData, int len) {
+	// receive package
+    memcpy(&inputData, incomingData, sizeof(inputData));
+    printf("Bytes received: %d\n",len);
+    printf("Function: %c\n",inputData.function);
+    printf("Value: %.2f\n",inputData.value);
 
-void callback(uint8_t src_mac[6], uint8_t *data, int len) {
-	handler->mypacket.wlan.actionframe.content.length = 127 + 5;
-	memcpy(handler->mypacket.wlan.actionframe.content.payload, data, 6);
-	//handler->set_dst_mac(dest_mac);
+    // send a response
+    outputData.function = 'M';
+    outputData.value = 2.3;
+
+	memcpy(handler->mypacket.wlan.actionframe.content.payload, &outputData, 8);
+	handler->mypacket.wlan.actionframe.content.length = 8+5;
+	handler->set_dst_mac(dest_mac);
 	handler->send();
-
-    printf("received %d chars long\n",len);
 }
 
 int main(int argc, char **argv) {
@@ -51,7 +57,7 @@ int main(int argc, char **argv) {
 
 	handler = new ESPNOW_manager(argv[1], DATARATE_24Mbps, CHANNEL_freq_1, my_mac, dest_mac, false);
 
-	//handler->set_filter(broadcastAddress, dest_mac);
+	handler->set_filter(dest_mac, my_mac);
 
 	handler->set_recv_callback(&callback);
 
